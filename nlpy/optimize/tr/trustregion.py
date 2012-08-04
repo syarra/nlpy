740670c302a4665c9e17d6a5dc9aa397e4c23c22
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2012-08-04 13:08:29 -0400

    Revert some changes in trustregion to use min max and absolute values from python and not from numpy

--- nlpy/optimize/tr/trustregion.py
+++ nlpy/optimize/tr/trustregion.py
@@ -58,8 +58,8 @@ class TrustRegionFramework:
         Compute the ratio of actual versus predicted reduction
         rho = (f - f_trial)/(-m)
         """
-        pred = -m + np.max(1.0, np.abs(f)) * 10.0 * self.eps
-        ared = f - f_trial + np.max(1.0, np.abs(f)) * 10.0 * self.eps
+        pred = -m + max(1.0, abs(f)) * 10.0 * self.eps
+        ared = f - f_trial + max(1.0, abs(f)) * 10.0 * self.eps
         if pred > 0 or not check_positive:
             return ared/pred
         else:
@@ -253,7 +253,7 @@ class TrustRegionBQP(TrustRegionSolver):
         TrustRegionSolver.__init__(self, g,  **kwargs)
 
         # self.bqpSolver = BQP(bqp, **kwargs)
-        self.bqpSolver = BQP_new(bqp, **kwargs)
+        self.bqpSolver = BQP(bqp, **kwargs)
 
         self.niter = 0
         self.stepNorm = 0.0
