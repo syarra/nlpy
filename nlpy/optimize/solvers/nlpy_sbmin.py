ee9a8d6d181cb69c9fa280ea713d90e03f5937ff
Dominique Orban <dominique.orban@gmail.com>
2012-08-05 16:37:09 -0400

    Add logger and simplify sbmin driver.

--- nlpy/optimize/solvers/nlpy_sbmin.py
+++ nlpy/optimize/solvers/nlpy_sbmin.py
@@ -5,6 +5,8 @@ from nlpy.optimize.tr.trustregion import TrustRegionFramework as TR
 from nlpy.optimize.tr.trustregion import TrustRegionBQP as TRSolver
 from nlpy.optimize.solvers.sbmin import SBMINFramework
 from nlpy.tools.timing import cputime
+from nlpy.tools.logs import config_logger
+import logging
 import numpy
 import sys
 
@@ -18,7 +20,8 @@ def pass_to_sbmin(nlp, showbanner=True):
     tr = TR(eta1=0.25, eta2=0.75, gamma1=0.0625, gamma2=2)
 
     # we select a trust-region subproblem solver of our choice.
-    SBMIN = SBMINFramework(nlp, tr, TRSolver, maxiter = 1000, verbose=False)
+    SBMIN = SBMINFramework(nlp, tr, TRSolver, maxiter=100, verbose=True,
+            ny=True, monotone=False, abstol=1.0e-8, reltol=1.0e-6)
     t_setup = cputime() - t                  # Setup time
 
     if showbanner:
@@ -50,7 +53,7 @@ def pass_to_sbmin(nlp, showbanner=True):
     print '  Total/Average BQP iter      : %-d/%-g' % (SBMIN.total_bqpiter, (float(SBMIN.total_bqpiter)/SBMIN.iter))
     print '  Setup/Solve time            : %-gs/%-gs' % (t_setup, SBMIN.tsolve)
     print '  Total time                  : %-gs' % (t_setup + SBMIN.tsolve)
-    print '  Status                      :', SBMIN.status 
+    print '  Status                      :', SBMIN.status
     print '-------------------------------'
     return SBMIN
 
@@ -61,26 +64,32 @@ if len(sys.argv) < 2:
 # Set printing standards for arrays
 numpy.set_printoptions(precision=3, linewidth=80, threshold=10, edgeitems=3)
 
+# Create root logger.
+log = logging.getLogger('nlpy.sbmin')
+level = logging.INFO
+log.setLevel(level)
+fmt = logging.Formatter('%(name)-15s %(levelname)-8s %(message)s')
+hndlr = logging.StreamHandler(sys.stdout)
+hndlr.setFormatter(fmt)
+log.addHandler(hndlr)
+
+# Configure subproblem logger.
+config_logger('nlpy.bqp',
+              filename='sbmin-bqp.log',
+              filemode='w',
+              stream=None)
+
+def apply_scaling(nlp):
+    "Apply scaling to the NLP and print something if asked."
+    gNorm = nlp.compute_scaling_obj()
+    log.info('%17s: %8s %8s %8s'     % ('Scaling applied', '|g| unscaled', '|g| scaled', 'component'))
+    log.info('%17s: %8.1e %8.1e'     % ('  objective', gNorm, nlp.scale_obj*gNorm))
+    log.info('')
+
 for ProblemName in sys.argv[1:]:
     nlp = amplpy.AmplModel(ProblemName)         # Create a model
-    print nlp.Uvar
-    print nlp.Lvar
+    nlp.display_basic_info()
+    apply_scaling(nlp)
     SBMIN = pass_to_sbmin(nlp, showbanner=True)
-    #nlp.writesol(SBMIN.x, nlp.pi0, 'And the winner is')    # Output "solution"
     nlp.close()                                 # Close connection with model
 
-# Plot the evolution of the trust-region radius on the last problem
-if SBMIN is not None:
-    try:
-        import pylab
-    except:
-        sys.stderr.write('If you had pylab installed, you would be looking ')
-        sys.stderr.write('at a plot of the evolution of the trust-region ')
-        sys.stderr.write('radius, right now.\n')
-        sys.exit(0)
-    radii = numpy.array(SBMIN.radii, 'd')
-    print radii
-    #pylab.plot(numpy.where(radii < 100, radii, 100))
-    pylab.plot(radii)
-    pylab.title('Trust-region radius')
-    pylab.show()
